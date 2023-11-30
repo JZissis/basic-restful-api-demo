7eb9a1f11f4de598f31059f1138b579110ef81c1
JZissis <1997giannis@gmail.com>
2023-12-01 01:14:46 +0200

    Interface methods for users HashMap

--- mobile-app-ws/src/main/java/com/demo/app/userservice/UserService.java
+++ mobile-app-ws/src/main/java/com/demo/app/userservice/UserService.java
@@ -1,8 +1,13 @@
 package com.demo.app.userservice;
 
+import java.util.Map;
+
 import com.demo.app.mobileappws.ui.model.request.UserDetailsRequestModel;
 import com.demo.app.ui.model.response.UserRest;
 
 public interface UserService {
     UserRest createUser(UserDetailsRequestModel userDetails);
+    UserRest getUserById(String userId);
+    Map<String, UserRest> getUsers();
+    void setUsers(Map<String, UserRest> users);
 }
